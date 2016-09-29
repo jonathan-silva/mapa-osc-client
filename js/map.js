78134e735aad711b77aca4b87f74ebe8fe14e889
Raul Sena Ferreira <raulsenaferreira@gmail.com>
2016-09-29 19:39:02 -0300

    Subtarefa 1 resolvida

--- js/map.js
+++ js/map.js
@@ -21,11 +21,52 @@ require(['jquery'], function(){
   var last_response_len = false;
   $.ajax({
       url: "js/pegadadosTeste.php",
-      data: { limite: '100000' },
+      data: { limite: '10' },
       type: "POST",
       dataType: "json",
       complete: function(dados) { //console.log(dados.responseText);
         console.log("FIM");
+        var lat=-16.55555555; var lng= -60.55555555;
+    var map = new L.Map('map', {center: new L.LatLng(lat, lng), zoom: 4});
+    var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    var ggl = new L.Google();
+    var ggl2 = new L.Google('RODMAP');
+    map.addLayer(ggl2);
+    var leafletView = new PruneClusterForLeaflet();
+    var size = 1000000;
+    var markers = [];
+    for (var i = 0; i < size; ++i) {
+    var latFinal = lat + (Math.random()- 0.0 ) * Math.random() * 0.00001 * size;
+    var lngFinal = lng + (Math.random() - 0.0) * Math.random() * 0.00002 * size;
+        var marker = new PruneCluster.Marker(latFinal, lngFinal);
+    /*marker.on('mouseover', function(event){
+    var marker = event.target;
+              var position = marker.getLatLng();
+    var panoramaOptions = {
+          position: position,
+          pov: {
+              heading: 34,
+              pitch: 10
+          }
+      };
+      var panorama = new  google.maps.StreetViewPanorama(document.getElementById('pano'),panoramaOptions);
+      map.setStreetView(panorama);
+    });*/
+    markers.push(marker);
+        leafletView.RegisterMarker(marker);
+    }
+    window.setInterval(function () {
+        for (i = 0; i < size / 2; ++i) {
+            var coef = i < size / 8 ? 10 : 1;
+            var ll = markers[i].position;
+            ll.lat += (Math.random() - 0.5) * 0.00001 * coef;
+            ll.lng += (Math.random() - 0.5) * 0.00002 * coef;
+        }
+        leafletView.ProcessView();
+    }, 3000);
+
+    map.addLayer(leafletView);
+    map.addControl(new L.Control.Layers( {'OSM':osm, 'Google':ggl, 'Google Terrain':ggl2}, {}));
       },
       progress: function(dados) {
         var this_response, response = dados.currentTarget.response;
--- js/pegadadosTeste.php
+++ js/pegadadosTeste.php
@@ -6,42 +6,22 @@
   $db_connection = pg_connect("host=10.1.4.110 dbname=mapaosc_test user=i3geo password=ipea001");
 
   $result = pg_query($db_connection, $SQLPontos);
-  $maxid = pg_fetch_row($result)[0];
+  $maxid = pg_fetch_row($result)[count($result)];
 
-  $json = array_fill(1, $maxid, '');
+  $json = array_fill(0, $maxid, '');
   while ($val = pg_fetch_row($result) ) {
     $json[$val[0]][0] = $val[1];//lat
     $json[$val[0]][1] = $val[2];//lng
   }
-  //$json = pg_fetch_all($result);
-  //print_r (json_encode($json));
+
   $chunks = count($json)/4;
   $pieceOfArray = array_chunk($json, $chunks, true);
 
-while(@ob_end_clean());
-foreach ($pieceOfArray as $key => $value) {
-  # code...
-
-  /*for($i=0;$i<count($pieceOfArray);$i++){
-
-    foreach ($pieceOfArray[$i] as $key => $value) {
-      echo(" $".$i."$ ".json_encode($value));
-      flush();
-      //ob_flush();
-    }
-  }
-    */
+  while(@ob_end_clean());
+  foreach ($pieceOfArray as $key => $value) {
     echo(json_encode($value));
     flush();
-    sleep(1);
+    //sleep(1);
+    //usleep(10000);
   }
-
-
-/*
-  for($i = 0; $i < $chunks; $i++){
-      print_r(json_encode($pieceOfArray[$i]));
-      //sleep(1);
-  }*/
-  //print_r(json_encode(array_chunk($json, $chunks, true)[0]));
-  //print_r (json_encode($json[2]));
 ?>
