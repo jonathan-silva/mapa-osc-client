996290b69cc2a20fe53b7913928b9d4859cb7496
Raul Sena Ferreira <raulsenaferreira@gmail.com>
2016-10-10 16:17:45 -0300

    Correção de rotas do arquivo resultado-consulta.js

--- js/resultado-consulta.js
+++ js/resultado-consulta.js
@@ -1,43 +1,3 @@
-/*require(['react', 'jsx!components/Util'], function (React) {
-
-  require(['componenteResultadoDaConsulta'], function(ResultadoConsulta){
-    function ResultadoDaConsulta(imagem,nome,cnpj,natjur,end){
-      this.imagem = imagem;
-      this.nome = nome;
-      this.cnpj = cnpj;
-      this.natjur = natjur;
-      this.end = end;
-    }
-
-    var imagem = ["img/camera.png"];
-    var nome = ["ASS DE MORADORES DA VILA MENDONCA"];
-    var cnpj = ["05.451.822/0001-02"];
-    var natjur = ["Associação Privada"];
-    var end = ["AVE MARANHAO 0, VILA MENDONCA, Floresta do Araguaia, PA, 68543000"];
-
-    var resultadoconsulta = [];
-    for (var i=0; i<nome.length; i++){
-      resultadoconsulta.push(new ResultadoDaConsulta(imagem[i],nome[i],cnpj[i],natjur[i],end[i]));//, formatos[i]));
-    }
-
-    ResultadoConsulta = React.createFactory(ResultadoConsulta);
-    ReactDOM.render(ResultadoConsulta({dados:resultadoconsulta}), document.getElementById("resultadoconsulta_formato_dados"));
-  });
-
-
-  /*require(['componenteDropdown'], function(Dropdown){
-    var arquivosRetornados, arquivosEnviados;
-    arquivosRetornados = arquivosEnviados = ["XML", "JSON", "CSV"];
-    var periodicidade = ["Dia(s)", "Semana(s)", "Mês(es)"];
-
-    Dropdown = React.createFactory(Dropdown);
-
-    ReactDOM.render(Dropdown({list: arquivosRetornados}), document.getElementById("arquivo_retornado_dropdown"));
-    ReactDOM.render(Dropdown({list:periodicidade}), document.getElementById("periodicidade_dropdown"));
-    ReactDOM.render(Dropdown({list:arquivosEnviados}), document.getElementById("tipo_arquivo_dropdown"));
-   });
-});
-*/
 require(['datatable'], function (React) {
   function tabela (newData){
     $('#resultadoconsulta_formato_dados').DataTable({
@@ -71,20 +31,123 @@ require(['datatable'], function (React) {
       console.log("Error");
     },
     success: function(data){
-      newData = new Array(data.length);
-        for (var i=0; i < data.length; i++){
-          newData[i] = new Array(6);
-          newData[i][0] = "<img class='img-circle media-object' src='img/camera.png' height='64' width='64'>";
-          newData[i][1] = data[i].tx_nome_osc;
-          newData[i][2] = data[i].cd_identificador_osc;
-          newData[i][3] = data[i].tx_natureza_juridica_osc;
-          newData[i][4] = data[i].tx_endereco_osc;
-          newData[i][5] = "<button type='button' class='btn btn-info' href='#'>Detalhar<span class='glyphicon glyphicon-search' aria-hidden='true'></span></button>";
-          /*("<img class='img-circle media-object' src='img/camera.png' height='64' width='64'>",
-        data[i].cd_identificador_osc, data[i].tx_nome_osc, data[i].tx_natureza_juridica_osc, data[i].tx_endereco_osc,
-        "<button type='button' class='btn btn-info' href='#'>Detalhar<span class='glyphicon glyphicon-search' aria-hidden='true'></span></button>");*/
-        }
-        tabela(newData);
+      var sizeOfData = data.length;
+      var columns = 6;
+      newData = new Array(sizeOfData);
+
+      for (var i=0; i < sizeOfData; i++){
+        newData[i] = new Array(columns);
+        newData[i][0] = "<img class='img-circle media-object' src='img/camera.png' height='64' width='64'>";
+        newData[i][1] = data[i].tx_nome_osc;
+        newData[i][2] = data[i].cd_identificador_osc;
+        newData[i][3] = data[i].tx_natureza_juridica_osc;
+        newData[i][4] = data[i].tx_endereco_osc;
+        newData[i][5] = "<button type='button' class='btn btn-info' href='#'>Detalhar<span class='glyphicon glyphicon-search' aria-hidden='true'></span></button>";
+        /*("<img class='img-circle media-object' src='img/camera.png' height='64' width='64'>",
+      data[i].cd_identificador_osc, data[i].tx_nome_osc, data[i].tx_natureza_juridica_osc, data[i].tx_endereco_osc,
+      "<button type='button' class='btn btn-info' href='#'>Detalhar<span class='glyphicon glyphicon-search' aria-hidden='true'></span></button>");*/
+      }
+      tabela(newData);
     }
   });
+});
+
+
+
+require(['jquery', 'rotas'], function(){
+  //carrega pontos em pedaços
+  (function addXhrProgressEvent($) {
+      var originalXhr = $.ajaxSettings.xhr;
+      $.ajaxSetup({
+          progress: function() { //console.log("standard progress callback");
+          },
+          xhr: function() {
+              var req = originalXhr(), that = this;
+              if (req) {
+                  if (typeof req.addEventListener == "function") {
+                      req.addEventListener("progress", function(evt) {
+                          that.progress(evt);
+                      },false);
+                  }
+              }
+              return req;
+          }
+      });
+  })(jQuery);
+
+
+    var last_response_len = false;
+    var lat=-16.55555555; var lng= -60.55555555;
+    var map = new L.Map('map', {center: new L.LatLng(lat, lng), zoom: 4});
+    var leafletView = new PruneClusterForLeaflet();
+
+    $.ajax({
+        url: "js/cacheConsulta.php",//trocar pela rota correta
+        data: { chave:'consultaTudo' },
+        type: "POST",
+        dataType: "json",
+        beforeSend: function() {
+          //var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+          //var ggl = new L.Google();
+          var ggl2 = new L.Google('RODMAP');
+          map.addLayer(ggl2);
+          //map.addControl(new L.Control.Layers( {'OSM':osm, 'Google':ggl, 'Google Terrain':ggl2}, {}));
+          map.addControl(new L.Control.Layers({'Google':ggl2}, {}));
+        },
+        complete: function(dados) {
+          //var pontos = '[{"2":["-15.7783899","-47.9286308"],"3":["-15.7993202","-47.8981781"]},{"4":["-15.7958345","-47.8923149"],"5":["-15.8199205","-47.9239616"]},{"6":["-15.7980804","-47.8906555"],"7":["-15.7952542779082","-47.9394622544892"]},{"11":["-15.8751554","-47.9755211"],"15":["-15.8104582","-47.8541069"]},{"16":["-15.793664","-47.8509483"]}]';
+          pontos = dados.responseJSON;
+          //console.log(pontos);
+          for(var k in pontos)
+            map.addLayer(loadPoint(k, pontos[k][0], pontos[k][1]));
+
+          leafletView.ProcessView();
+        },
+        progress: function(dados) {
+          var this_response, response = dados.currentTarget.response;
+          if(last_response_len === false)
+          {
+              this_response = response;
+              last_response_len = response.length;
+          }
+          else
+          {
+              this_response = response.substring(last_response_len);
+              last_response_len = response.length;
+          }
+
+          console.log("Loading...");
+        }
+    });
+
+    function loadPoint(id, latFinal, lngFinal){
+      if(latFinal !="" || lngFinal != ""){
+        var marker = new PruneCluster.Marker(latFinal, lngFinal);
+        marker.data.ID = id;
+
+        leafletView.PrepareLeafletMarker = function(leafletMarker, data) {
+            leafletMarker.on('click', function(){
+              carregaOSC(data.ID, leafletMarker);
+            });
+        };
+
+        leafletView.RegisterMarker(marker);
+        return leafletView;
+      }
+    }
+
+  function carregaOSC(id, leafletMarker){
+    var rotas = new Rotas();
+      $.ajax({
+          url: rotas.OSCByID(id),
+          type: "GET",
+          dataType: "json",
+          complete: function(data){
+            //console.log(data);
+            var cabecalho = data.responseJSON === undefined ? undefined : data.responseJSON.cabecalho;
+            var idOSC = cabecalho === undefined ? "OSC ainda não registrada no banco" : cabecalho.cd_identificador_osc;
+            leafletMarker.bindPopup('Codigo identificador da OSC= '+idOSC).openPopup();
+          }
+    });
+  }
 });
