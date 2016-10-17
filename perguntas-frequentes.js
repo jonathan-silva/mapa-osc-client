b9367141a2fbd8db98fe99d16fe40e5b95a66fa2
tmnascimento <tmnascimento@gmail.com>
2016-10-17 19:22:43 -0200

    Atualização da Página de Metodologia

---
+++ perguntas-frequentes.js
@@ -0,0 +1,5 @@
+var html = ["<b>Tiago</b>", "<i>Raul</i>"];
+var pergunta = ['titulo1','titulo2'];
+for (var i=0; i<html.length; i++)
+    $(".periodicidade").append(pergunta[i]+html[i]);
+[<div class=​"periodicidade" id=​"periodicidade_dropdown">​…​</div>​]
