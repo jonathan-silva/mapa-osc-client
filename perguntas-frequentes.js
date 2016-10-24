7e1da130d0c170a39d578fef49725a8dbbd62bdc
Pontes Cotta <pontescotta@gmail.com>
2016-10-24 18:59:34 -0200

    Issue: [MOSC #841] Subject:adicionar caminho de navegação nas paginas Detail:

--- perguntas-frequentes.js
+++
@@ -1,5 +0,0 @@
-var html = ["<b>Tiago</b>", "<i>Raul</i>"];
-var pergunta = ['titulo1','titulo2'];
-for (var i=0; i<html.length; i++)
-    $(".periodicidade").append(pergunta[i]+html[i]);
-[<div class=​"periodicidade" id=​"periodicidade_dropdown">​…​</div>​]
