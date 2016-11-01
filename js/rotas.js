a94a0b3a255b67ad058d45ec2b14d24a5ea192ba
Raul Sena Ferreira <raulsenaferreira@gmail.com>
2016-11-01 18:43:32 -0200

    Correção de link para o repositorio

--- js/rotas.js
+++ js/rotas.js
@@ -4,54 +4,54 @@ class Rotas{
   }
 
   static get getBaseUrl(){
-    return "http://localhost:8383";
+    return "mapaosc-desenv.ipea.gov.br:8383";
   }
 
   //index.js
   AutocompleteOSCByName(term, limit){
-    return "http://localhost:8383"+"/api/search/osc/autocomplete/"+term+"/"+limit;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/osc/autocomplete/"+term+"/"+limit;
   }
   AutocompleteOSCByCounty(term){
-    return "http://localhost:8383"+"/api/menu/geo/municipio/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/menu/geo/municipio/"+term;
   }
   AutocompleteOSCByState(term){
-    return "http://localhost:8383"+"/api/menu/geo/estado/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/menu/geo/estado/"+term;
   }
   AutocompleteOSCByRegion(term){
-    return "http://localhost:8383"+"/api/menu/geo/regiao/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/menu/geo/regiao/"+term;
   }
 
   //resultado-consulta.js
   OSCByID(id){
-    return "http://localhost:8383"+"/api/osc/"+id;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/osc/"+id;
   }
   OSCByNameInMap(term){
-    return "http://localhost:8383"+"/api/search/osc/geo/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/osc/geo/"+term;
   }
   OSCByCountyInMap(term){
-    return "http://localhost:8383"+"/api/search/municipio/geo/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/municipio/geo/"+term;
   }
   OSCByStateInMap(term){
-    return "http://localhost:8383"+"/api/search/estado/geo/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/estado/geo/"+term;
   }
   OSCByRegionInMap(term){
-    return "http://localhost:8383"+"/api/search/regiao/geo/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/regiao/geo/"+term;
   }
 
   OSCByName(term){
-    return "http://localhost:8383"+"/api/search/osc/lista/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/osc/lista/"+term;
   }
   OSCByCounty(term){
-    return "http://localhost:8383"+"/api/search/municipio/lista/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/municipio/lista/"+term;
   }
   OSCByState(term){
-    return "http://localhost:8383"+"/api/search/estado/lista/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/estado/lista/"+term;
   }
   OSCByRegion(term){
-    return "http://localhost:8383"+"/api/search/regiao/lista/"+term;
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/search/regiao/lista/"+term;
   }
 
   ClusterEstado(){
-    return "http://localhost:8383"+"/api/geo/cluster/estado";
+    return "mapaosc-desenv.ipea.gov.br:8383"+"/api/geo/cluster/estado";
   }
 }
