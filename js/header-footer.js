a2b0eb74d35afde0a5953c9b0a69a7ed3f6f1c41
Heraldo Pimenta Borges Filho <B209193861@ipea.gov.br>
2017-01-27 18:33:27 -0200

    Issue: [MOSC #974]

--- js/header-footer.js
+++ js/header-footer.js
@@ -49,7 +49,9 @@ require(['react', 'jsx!components/Header','bootstrap'], function(React, Header) 
 //  linksUsuarioLogado.push(new MenuDropDownObject("Adicionar Dados", "entrada-dados.html"));
 //  linksUsuarioLogado.push(new MenuDropDownObject("Adicionar Edital", "adicionar-edital.html"));
   linksUsuarioLogado.push(new MenuDropDownObject("",""));
-  linksUsuarioLogado.push(new MenuDropDownObject("Sair", "javascript:deslogar();"));
+  linksUsuarioLogado.push(new MenuDropDownObject("Sair", "javascript: deslogar();void(0);"));
+
+
 
   Header = React.createFactory(Header);
   ReactDOM.render(Header({headerObject: new Menu(linksSubmenu, menuList, linksUsuarioLogado, linksSubmenuDados)}), document.getElementById("header"));
