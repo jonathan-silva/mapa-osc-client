b6afe6aafa4217807337e6f508f7f2a92f614448
pontescotta <pontescotta@gmail.com>
2018-01-19 12:50:33 -0200

    MOSC-1453

--- dev/js/custom/links-uteis.js
+++ dev/js/custom/links-uteis.js
@@ -21,7 +21,7 @@ require(["jquery-ui"], function (React) {
           $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
      });
   });
-  
+
 });
 
 require(['react', 'jsx!components/Util'], function (React) {
@@ -40,13 +40,13 @@ require(['react', 'jsx!components/Util'], function (React) {
 //var elo = ["#"]
     var titulo = ["Portal de Convênios","Biblioteca Digital das OSCs","Biblioteca do Marco Regulatório das OSCs","Pesquisa FASFIL 2010","Participa.br","Marco Regulatório das OSCs","Sniic - Sistema Nacional de Informações e Indicadores Culturais"];
     var desc = [
-    "O Portal dos Convênios é o sítio eletrônico que abriga o Sistema de Gestão de Convênios e Contratos de Repasse – SICONV, bem como todo o conjunto de informações relacionadas à Convênios e Contratos de Repasse.",
-    "Biblioteca da Associação Brasileira de Organizações Não Governamentais - Abong",
-    "Estudos e Pesquisas sobre OSCs",
-    "Pesquisa sobre Fundações Privadas e Associações sem Fins Lucrativos - FASFIL",
-    "O Participa.br é uma Plataforma de Participação Social. Trata-se de mais um espaço para participação social no Brasil, escuta e diálogo entre o Governo Federal e a Sociedade Civil.",
+    "O Portal dos Convênios é a página virtual que abriga o Sistema de Gestão de Convênios e Contratos de Repasse – SICONV, bem como todo o conjunto de informações relacionadas a convênios e contratos de repasse.",
+    "Página oficial da Biblioteca da Associação Brasileira de Organizações Não Governamentais – ABONG.",
+    "A Biblioteca do Marco Regulatório das OSCs reúne estudos e pesquisas sobre OSCs.",
+    "Pesquisa sobre Fundações Privadas e Associações sem Fins Lucrativos – FASFIL.",
+    "O Participa.br é uma Plataforma de Participação Social. Trata-se de um espaço virtual para participação social no Brasil, com o objetivo de facilitar o diálogo entre o Governo Federal e a Sociedade Civil.",
     "O Marco Regulatório das Organizações da Sociedade Civil é fruto de um esforço conjunto do governo federal e da sociedade civil para modernizar as relações do poder público com as Organizações da Sociedade Civil.",
-    "O Sistema Nacional de Informações e Indicadores Culturais é um banco de dados disponível para toda a sociedade. Ele disponibliza dados de bens, serviços, infraestrutura, investimentos, instituições e gestão cultural, produção, acesso, consumo, agentes, programas e transparência."
+    "O Sistema Nacional de Informações e Indicadores Culturais é um banco de dados disponível para toda a sociedade. Ele reúne dados de bens, serviços, infraestrutura, investimentos, instituições e gestão cultural, produção, acesso, consumo, agentes, programas e transparência."
   ];
     var imagem = ["img/siconv.png","img/abong.png","img/mrosc.png","img/fasfil-2010.png","img/participa-br.png","img/icone-mrosc.png","img/sniic.png"];
     var elo = ["http://portal.convenios.gov.br/","http://bibliotecadigital.abong.org.br/","http://www.secretariadegoverno.gov.br/iniciativas/mrosc/estudos-e-pesquisas",
