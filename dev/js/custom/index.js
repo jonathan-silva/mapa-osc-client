2c7da46c20cdec1c084ef372a02e7d3a3b6d2899
pontescotta <pontescotta@gmail.com>
2018-05-28 10:30:27 -0300

    MOSC-1636

--- dev/js/custom/index.js
+++ dev/js/custom/index.js
@@ -150,11 +150,38 @@ require(["nv.d3.lib","graficoParaTabela"], function (React) {
         }
         ]}];
 
+  var jsonGrafico6 = [{"config":[",.1f",1000000000,""],"leg_X":"Ano","leg_Y":"em Bilhoes R$","tituloColuna":["Entidade", "Ano", "Valor Total"],"legenda":"Fonte: Senado Federal (2018). Elaboração própria. Nota: Referem-se aos valores executados (liquidados). Valores corrigidos para preços de janeiro de 2018 pelo IPCA/IBGE. Para calcular o valor do orçamento, foram excluídos os valores destinados ao serviço da dívida pública.","titulo":"Evolução de recursos públicos federais transferidos para entidades sem fins lucrativos e somente para OSCs (2010-2017)",
+  series:[
+        {
+          "tipo_valor":"$",
+          values: [{"x" : 2010, "y" : 6278480452 }, {"x" : 2011, "y" : 4898668291 },
+          {"x" : 2012, "y" : 6688450140 }, {"x" : 2013, "y" : 8196459734}, {"x" : 2014, "y" : 8265271851},
+           {"x" : 2015, "y" : 6744253807 }, {"x" : 2016, "y" : 4901355349 }, {"x" : 2017, "y" : 3252559195 }],
+          key: 'ESFLs'
+        },
+        {
+          "tipo_valor":"$",
+          values: [{"x" : 2010, "y" : 11440189630.5479 }, {"x" : 2011, "y" : 10054475412.4703 },
+          {"x" : 2012, "y" : 10607810116.9065 }, {"x" : 2013, "y" : 11099497076.5084 }, {"x" : 2014, "y" : 12112818316.924 },
+           {"x" : 2015, "y" : 11368502473.1809 }, {"x" : 2016, "y" : 2312173689.10068 }, {"x" : 2017, "y" : 6352490520.4482 }],
+          key: 'OSCs'
+        },
+        {
+          "tipo_valor":"$",
+          values: [{"x" : 2010, "y" : 2678099914.85083 }, {"x" : 2011, "y" : 1640846786.21371 },
+          {"x" : 2012, "y" : 2433787954.02355 }, {"x" : 2013, "y" : 2664537618.54551 }, {"x" : 2014, "y" : 2498533417.99339 },
+           {"x" : 2015, "y" : 1787719861.38991 }, {"x" : 2016, "y" : 353510128.429499 }, {"x" : 2017, "y" : 2229340028.59652 }],
+          key: 'OSCs (Modalidade 50)'
+        }
+  ]}];
+
   createMultiBarChart('#graficoMain-1',jsonGrafico1);
 //  createLinePlusBarChart('#graficoMain-2',jsonGrafico2);
 //  createLineChart('#graficoMain-3',jsonGrafico3);
   createBarChart('#graficoMain-4',jsonGrafico4);
   createLinePlusBarChart('#graficoMain-5',jsonGrafico5);
+  createLineChart('#graficoMain-6',jsonGrafico6);
+
 
   $("#tabelaMain-1").click(function(){
       createTabela_MultBar_Line(jsonGrafico1,false);
@@ -174,6 +201,10 @@ require(["nv.d3.lib","graficoParaTabela"], function (React) {
 
   $("#tabelaMain-5").click(function(){
       createTabela_MultBar_Line(jsonGrafico5,false);
+  });
+
+  $("#tabelaMain-6").click(function(){
+      createTabela_MultBar_Line(jsonGrafico6,true);
   });
 
 } );
