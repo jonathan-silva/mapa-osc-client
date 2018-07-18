5a7a18d5bd722558779a2a82f548f1bd611e5243
tmnascimento <tmnascimento@git.com>
2018-07-18 22:35:38 +0000

    MOSC-1693 - Erro na verificacao do CPF

--- dev/js/custom/cadastro-representante.js
+++ dev/js/custom/cadastro-representante.js
@@ -349,6 +349,32 @@ function validaCNPJ(cnpj) {
 }
 
 function validaCPF(cpf) {
+  var Soma;
+    var Resto;
+    Soma = 0;
+    //strCPF  = RetiraCaracteresInvalidos(strCPF,11);
+    var exp = /\.|\-/g ;
+    cpf = cpf.toString().replace(exp, "");
+    if (cpf == "00000000000")
+	return false;
+    for (var i=1; i<=9; i++)
+	Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+	Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) )
+	return false;
+	Soma = 0;
+    for (var i = 1; i <= 10; i++)
+       Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11))
+	Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) )
+        return false;
+    return true;
+
+  /*
     var exp = /\.|\-/g
     cpf = cpf.toString().replace(exp, "");
     var digitoDigitado = eval(cpf.charAt(9) + cpf.charAt(10));
@@ -370,6 +396,7 @@ function validaCPF(cpf) {
     } else {
         return true;
     }
+    */
 }
 
 
