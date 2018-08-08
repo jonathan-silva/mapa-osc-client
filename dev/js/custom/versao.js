f387e4b4306b68f2146a5161a8051a57baf336e7
vagnerpraia <vagnerpraia@gmail.com>
2018-08-08 19:46:19 -0300

    Issue: MOSC-1685 Subject: Implantação da API e do front em apenas uma porta Detail: Acerto no versao.js

--- dev/js/custom/versao.js
+++ dev/js/custom/versao.js
@@ -27,10 +27,9 @@ require(['rotas',"jquery-ui"], function (React) {
   var modulo = "versao";
 
   $.ajax({
-    url: 'js/controller.php',
+    url: rotas.ModuloBySlug(modulo),
     type: 'GET',
     dataType: 'json',
-    data: {flag: 'consulta', rota: rotas.ModuloBySlug(modulo)},
     error: function(e){
         console.log("ERRO no AJAX :" + e);
         $('.manutencao').css('display', 'block');
