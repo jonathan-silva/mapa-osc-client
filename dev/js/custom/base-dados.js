301ff556af97cc00ead5def4a3200bd8aee05f1c
pontescotta <pontescotta@gmail.com>
2018-10-17 15:25:09 -0300

    MOSC-1747-links

--- dev/js/custom/base-dados.js
+++ dev/js/custom/base-dados.js
@@ -16,10 +16,40 @@ require(["jquery-ui"], function (React) {
   });
 
   jQuery(document).ready(function($) {
-      $(".scroll").click(function(event){
+
+    var dataAtual = new Date();
+    var mes = getMesExtenso(dataAtual.getMonth());
+    var ano = dataAtual.getFullYear();
+    mostrarData(mes, ano);
+
+    $(".scroll").click(function(event){
           event.preventDefault();
           $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
      });
+
   });
-  
+
+function mostrarData(mes, ano){
+    var retorno = mes + " de " + ano;
+    document.getElementById("datahora").innerHTML = retorno;
+}
+
+function getMesExtenso(mes){
+    var arrayMes = new Array(12);
+    arrayMes[0] = "Janeiro";
+    arrayMes[1] = "Fevereiro";
+    arrayMes[2] = "Março";
+    arrayMes[3] = "Abril";
+    arrayMes[4] = "Maio";
+    arrayMes[5] = "Junho";
+    arrayMes[6] = "Julho";
+    arrayMes[7] = "Agosto";
+    arrayMes[8] = "Setembro";
+    arrayMes[9] = "Outubro";
+    arrayMes[10] = "Novembro";
+    arrayMes[11] = "Dezembro";
+
+    return arrayMes[mes];
+}
+
 });
