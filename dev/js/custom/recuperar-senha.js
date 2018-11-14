4770be7ce0e72ce34428665c89465bcc0dd4ee19
vagnerpraia <vagnerpraia@gmail.com>
2018-11-14 17:25:30 -0200

    Alteração do ajax do alterar senha

--- dev/js/custom/recuperar-senha.js
+++ dev/js/custom/recuperar-senha.js
@@ -71,15 +71,13 @@ require(["jquery-ui", "rotas"], function(React) {
              data: json,
              error:function(data){
                  jQuery("#modalTitle").text("Problema na solicitação!");
-                 jQuery("#modalConteudo").text("Não foi possível recuperar a senha.");
-                 //jQuery("#modalConteudo").text(JSON.parse(data.responseText).msg);
+                 jQuery("#modalConteudo").text(data.responseJSON.msg);
                  $modal.modal('show');
                  return false;
              },
              success: function(data){
                jQuery("#modalTitle").text("Recuperar Senha");
-               jQuery("#modalConteudo").text("Foi enviado um e-mail para a troca da senha");
-               //jQuery("#modalConteudo").text(JSON.parse(data.responseText).msg);
+               jQuery("#modalConteudo").text(data.msg);
                $modal.modal('show');
              }
          }); //final envio ajax
